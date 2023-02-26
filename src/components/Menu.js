238f51bd04a59b4f3ae8c7cf15bae4487afb9923
Aristotle <knight777@hotmail.ca>
2023-02-26 03:19:37 -0500

    mobile navbar done

---
+++ src/components/Menu.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { HiBarsArrowUp } from 'react-icons/hi2'
+import { motion } from 'framer-motion'
+
+
+function Menu() {
+
+    const [isOpen, setIsOpen] = useState(false)
+
+
+    return (
+        <div className='flex'>
+            <motion.div
+                initial={{
+                    x: 300,
+                    opacity: 0,
+                    scale: 0.5
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1,
+                    scale: 1
+                }}
+                transition={{
+                    duration: 1.8
+                }}
+                className='flex-row space-x-4 hidden sm:inline-flex'
+            >
+                <h1>Projects</h1>
+                <h1>Experience</h1>
+                <h1>Algo Expert</h1>
+
+            </motion.div>
+
+
+            <motion.div
+                initial={{
+                    x: 300,
+                    opacity: 0,
+                    scale: 0.5
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1,
+                    scale: 1
+                }}
+                transition={{
+                    duration: 1.8
+                }}
+                className='flex-col sm:hidden'
+            >
+                {
+                    //  
+                }
+                <GiHamburgerMenu
+                    className={` w-6 h-6 z-10 transition-all ${isOpen ? 'text-[#43485400]' : 'text-[#434854]'} duration-700 `}
+                    onClick={() => setIsOpen(!isOpen)}
+                />
+
+
+                <motion.div
+                    className='menu absolute flex w-screen items-center h-auto left-0 right-0 top-0 bg-[#ffffff73] justify-center'
+                    initial={{
+
+                        opacity: 0,
+                    }}
+                    animate={{
+                        y: (isOpen ? 0 : -250),
+                        opacity: 0.5,
+                    }}
+                    transition={{
+                        duration: 1
+                    }}
+                >
+                    <div className='flex flex-col text-black text-center text-[25px] font-tilt-prism items-center p-2 pb-5 space-y-2'>
+                        <HiBarsArrowUp className='h-8 w-8 mt-3' onClick={() => setIsOpen(!isOpen)} />
+                        <a className='border-b border-gray-50' href=""><h1>Projects</h1></a>
+                        <a className='border-b border-gray-50' href=""><h1>Experience</h1></a>
+                        <a className='border-b border-gray-50' href=""><h1>Algo Expert</h1></a>
+                    </div>
+                </motion.div>
+            </motion.div>
+        </div>
+    )
+}
+
+export default Menu
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
 import { motion } from 'framer-motion'
 import { SocialIcon } from 'react-social-icons'
-import { GiHamburgerMenu } from 'react-icons/gi'
+import Menu from './Menu'
 
 function Navbar() {
 
-  const [isOpen, setIsOpen] = useState(false)
+  
 
   return (
     <div className='sticky top-0 p-5 flex justify-between items-center max-w-4xl mx-auto'>
@@ -38,83 +38,7 @@ function Navbar() {
           bgColor='transparent'
         />
       </motion.div>
-      <motion.div
-        initial={{
-          x: 300,
-          opacity: 0,
-          scale: 0.5
-        }}
-        animate={{
-          x: 0,
-          opacity: 1,
-          scale: 1
-        }}
-        transition={{
-          duration: 1.8
-        }}
-        className='flex-row space-x-4 hidden md:inline-flex'
-      >
-        <h1>Projects</h1>
-        <h1>Experience</h1>
-        <h1>Algo Expert</h1>
-
-      </motion.div>
-
-
-      <motion.div
-        initial={{
-          x: 300,
-          opacity: 0,
-          scale: 0.5
-        }}
-        animate={{
-          x: 0,
-          opacity: 1,
-          scale: 1
-        }}
-        transition={{
-          duration: 1.8
-        }}
-        className='flex-row space-x-4 sm:hidden'
-      >
-        {
-          //  w-6 h-6 ${ isOpen ? 'text-[#43485480]' : 'text-[#434854]'}
-        }
-        <GiHamburgerMenu
-          className={` w-6 h-6 text-[#434854] focus:opacity-100 active:opacity-50 transition-opacity duration-1000`}
-          onClick={() => setIsOpen(!isOpen)}
-        />
-
-        {isOpen && (
-          <motion.div
-            className='fixed right-0 p-5'
-            initial={{
-              y: -50,
-              opacity: 0.5,
-              rotateX: -90
-            }}
-            animate={{
-              y: 0,
-              opacity: 1,
-              rotateX: 0
-            }}
-            transition={{
-              duration: 0.5
-            }}
-          >
-            <div className='w-50'>
-              {
-                // TODO: style this more appropriately
-              }
-              <ul className='border rounded-lg border-solid border-white p-3 text-center'>
-                <li>Projects</li>
-                <li>Experience</li>
-                <li>Algo Expert</li>
-              </ul>
-            </div>
-          </motion.div>
-        )}
-      </motion.div>
+      <Menu />
     </div>
   )
 }
